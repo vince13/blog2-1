af78d2ab31e9aa3daf39ba416c17c43892da6d78
vince13 <hello@vincentibekwe.com>
2024-01-25 23:25:47 +0100

    update settings

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ["new-blog.up.railway.app"]
+ALLOWED_HOSTS = [""]
 
 LOGIN_URL = "userauths:login"
 LOGIN_REDIRECT_URL = "blog:home"
